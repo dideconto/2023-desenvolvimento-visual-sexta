857690d0cefc21e0ebbde5327bece40dae793aff
Diogo <dideconto@gmail.com>
2023-09-04 17:03:45 -0300

    API Ecommerce - Configuração do EF com SQLite

---
+++ API/WebApi/Data/AppDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data;
+
+public class AppDataContext : DbContext
+{
+    public AppDataContext(DbContextOptions<AppDataContext> options) : base(options) { }
+
+    //Classes que vão virar tabelas no banco de dados
+    public DbSet<Produto> Produtos { get; set; }
+}
