342d59de8e8a50b31de72fe0aff790a3c63ae6c2
Diogo Deconto <dideconto@gmail.com>
2023-11-10 10:56:06 -0300

    FRONT ECOMMERCE - Material Design

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -1,28 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ListarProdutoComponent } from './pages/produto/listar-produto/listar-produto.component';
-import { CadastrarProdutoComponent } from './pages/produto/cadastrar-produto/cadastrar-produto.component';
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ListarProdutoComponent } from "./pages/produto/listar-produto/listar-produto.component";
+import { CadastrarProdutoComponent } from "./pages/produto/cadastrar-produto/cadastrar-produto.component";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   //Componentes da aplicação
   declarations: [
     AppComponent,
     ListarProdutoComponent,
-    CadastrarProdutoComponent
+    CadastrarProdutoComponent,
   ],
   //Bibliotecas externas da aplicação
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
